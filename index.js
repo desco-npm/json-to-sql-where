dc5555e95c003baf52c022f5a5426fab90385f74
Rafael Dias <contato@diasrafael.com.br>
2018-09-01 15:29:30 -0300

    (1==1) quando vazio

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const objectMap = require('object-map')
 const quotesIfText = require('@desco/quotes-if-text')
 
 const jsonToSqlWhere = (json = {}, operator = 'AND') => {
+    if (Object.keys(json).length === 0) return '(1 == 1)'
+
     const string = objectMap(json, (val, key) => {
         if (typeof val !== 'object') {
             return `${key} = ${quotesIfText(val)}`
