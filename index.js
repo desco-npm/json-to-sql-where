54931e673578cf246a4d78da0071c872188bd30a
Rafael Dias <contato@diasrafael.com.br>
2018-09-04 12:00:02 -0300

    Operadores

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const jsonToSqlWhere = (json = {}, operator = 'AND') => {
     if (Object.keys(json).length === 0) return '(1 = 1)'
 
     const string = objectMap(json, (val, key) => {
-        if (typeof val !== 'object') {
-            return `${key} = ${quotesIfText(val)}`
+        if (typeof val !== 'object' || val.length >= 0) {
+            if (typeof val !== 'object') val = [ '=', val, ]
+
+            if (val[0] === 'IN') {
+                val[1] = `(${val[1]})`
+            } else {
+                val[1] = quotesIfText(val[1])
+            }
+
+            return `${key} ${val[0]} ${val[1]}`
         }
         else {
             return jsonToSqlWhere(val, key)
