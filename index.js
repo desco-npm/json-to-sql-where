f287f071065ddad9d35a994f08b1c17ad65b89b0
eudiasrafael <eu@diasrafael.com.br>
2020-08-05 14:11:26 -0300

    BETWEEN

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const objectMap = require('object-map')
 const quotesIfText = require('@desco/quotes-if-text')
+
+const objectMap = require('object-map')
 const isObject = require('is-object')
 
 const jsonToSqlWhere = (_json = {}, _operator = 'AND') => {
-    const conditions = []
+  const conditions = []
 
-    objectMap(_json, (v, k) => {
-        if ([ 'AND', 'OR', 'XOR', ].indexOf(k) === -1) {
-            if (!isObject(v)) {
-                v = { type: '=', value: v, }
-            }
+  objectMap(_json, (v, k) => {
+    if ([ 'AND', 'OR', 'XOR', ].indexOf(k) === -1) {
+      if (!isObject(v)) {
+        v = { type: '=', value: v, }
+      }
 
-            if (typeof v.value === 'string') {
-                v.value = quotesIfText(v.value)
-            }
+      if (typeof v.value === 'string') {
+        v.value = quotesIfText(v.value)
+      }
 
-            if (v.type === 'IN') {
-                v.value = `(${v.value.map(v => quotesIfText(v)).join(',')})`
-            }
+      if (v.type === 'IN') {
+        v.value = `(${v.value.map(v => quotesIfText(v)).join(',')})`
+      }
 
-            conditions.push(`${k} ${v.type} ${v.value}`)
-        }
-        else {
-            conditions.push(` (${jsonToSqlWhere(v, k)}) `)
-        }
-    })
+      if (v.type === 'BETWEEN') {
+        v.value = `${v.value[0]} AND ${v.value[1]}`
+      }
 
-    return conditions.join(` ${_operator} `)
+      conditions.push(`${k} ${v.type} ${v.value}`)
+    }
+    else {
+      conditions.push(` (${jsonToSqlWhere(v, k)}) `)
+    }
+  })
+
+  return conditions.join(` ${_operator} `)
 }
 
 module.exports = jsonToSqlWhere
